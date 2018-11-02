1a91cc3dcd7627abbbdf277de24e2cade97d7f28
liambrdy <liam.james.brady@gmail.com>
2018-11-02 17:21:03 -0400

    added printInfo()

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,75 @@
 #include <vulkan/vulkan.h>
+#include <iostream>
+
+#define ASSERT_VULKAN(val)                                                  \
+    if (val != VK_SUCCESS)                                                  \
+    {                                                                       \
+        std::cerr << "Error VULKAN::INSTANCE_CREATION_FAILED" << std::endl; \
+        exit(EXIT_FAILURE);                                                 \
+    }
+
+VkInstance instance;
+
+void printStats(VkPhysicalDevice &device)
+{
+    VkPhysicalDeviceProperties deviceProps;
+    vkGetPhysicalDeviceProperties(device, &deviceProps);
+
+    uint32_t apiVer = deviceProps.apiVersion;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Name:               " << deviceProps.deviceName << std::endl;
+    std::cout << "API Version:        " << VK_VERSION_MAJOR(apiVer) << "." << VK_VERSION_MINOR(apiVer) << "." << VK_VERSION_PATCH(apiVer) << std::endl;
+    std::cout << "Driver Version:     " << deviceProps.driverVersion << std::endl;
+    std::cout << "Vendor ID:          " << deviceProps.vendorID << std::endl;
+    std::cout << "Device ID:          " << deviceProps.deviceID << std::endl;
+    std::cout << "Device Type:        " << deviceProps.deviceType << std::endl;
+
+    std::cout << std::endl;
+
+    VkPhysicalDeviceFeatures deviceFeatures;
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+    std::cout << "Geometry Shader:    " << deviceFeatures.geometryShader << std::endl;
+
+    std::cout << std::endl;
+
+    VkPhysicalDeviceMemoryProperties deviceMemProps;
+    vkGetPhysicalDeviceMemoryProperties(device, &deviceMemProps);
+    std::cout << "Memory Type:        " << deviceMemProps.memoryTypes << std::endl;
+    std::cout << "Memory Heaps:       " << deviceMemProps.memoryHeaps << std::endl;
+
+    std::cout << std::endl;
+
+    uint32_t familyQueueCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &familyQueueCount, NULL);
+    VkQueueFamilyProperties *familyProperties = new VkQueueFamilyProperties[familyQueueCount];
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &familyQueueCount, familyProperties);
+
+    std::cout << "Queue Family Count: " << familyQueueCount << std::endl;
+
+    for (int i = 0; i < familyQueueCount; i++)
+    {
+        std::cout << std::endl;
+        std::cout << "Queue Family # " << i << std::endl;
+        std::cout << "VK_QUEUE_GRAPHICS_BIT       " << ((familyProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) != 0) << std::endl;
+        std::cout << "VK_QUEUE_COMPUTE_BIT        " << ((familyProperties[i].queueFlags & VK_QUEUE_COMPUTE_BIT) != 0) << std::endl;
+        std::cout << "VK_QUEUE_TRANSFER_BIT       " << ((familyProperties[i].queueFlags & VK_QUEUE_TRANSFER_BIT) != 0) << std::endl;
+        std::cout << "VK_QUEUE_SPARSE_BINDING_BIT " << ((familyProperties[i].queueFlags & VK_QUEUE_SPARSE_BINDING_BIT) != 0) << std::endl;
+        std::cout << "Queue Count:          " << familyProperties[i].queueCount << std::endl;
+        std::cout << "Timestamp Valid Bits: " << familyProperties[i].timestampValidBits << std::endl;
+        uint32_t width = familyProperties[i].minImageTransferGranularity.width;
+        uint32_t height = familyProperties[i].minImageTransferGranularity.height;
+        uint32_t depth = familyProperties[i].minImageTransferGranularity.depth;
+        std::cout << "Min Image Timestamp Granularity : " << width << ", " << height << ", " << depth << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    delete[] familyProperties;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -19,6 +90,24 @@ int main(int argc, char const *argv[])
     instanceInfo.ppEnabledLayerNames = NULL;
     instanceInfo.enabledExtensionCount = 0;
     instanceInfo.ppEnabledExtensionNames = NULL;
+
+    VkResult result = vkCreateInstance(&instanceInfo, NULL, &instance);
+
+    ASSERT_VULKAN(result);
+
+    uint32_t deviceCount = 0;
+    result = vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+    ASSERT_VULKAN(result);
+
+    VkPhysicalDevice *physicalDevices = new VkPhysicalDevice[deviceCount];
+
+    result = vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices);
+    ASSERT_VULKAN(result);
+
+    for (int i = 0; i < deviceCount; i++)
+    {
+        printStats(physicalDevices[i]);
+    }
 
     return 0;
 }
